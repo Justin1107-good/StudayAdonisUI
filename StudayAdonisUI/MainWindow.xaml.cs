63a96ef4a96642ea3f9f574c56e5f6ff2d370812
justin wang <justin.wang@skycoresaas.com>
2022-08-26 11:37:44 +0800

    添加项目文件。

---
+++ StudayAdonisUI/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudayAdonisUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 添加姓名
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            //确保文本框包含名称。
+            //验证文本框中输入的名称是否已经存在。
+            //将名称添加到列表框。
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("名称不能为空！");
+                return;
+            }
+        }
+    }
+}
